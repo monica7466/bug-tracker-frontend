5a2077dcc19eaf8f099076ec76854818edc61a45
monica7466 <monica.kwadagi@capgemini.com>
2021-10-14 02:07:15 +0530

    com

--- src/components/Users/UserRegister.js
+++ src/components/Users/UserRegister.js
@@ -1,4 +1,96 @@
+// import React, { useEffect, useState } from 'react';
+// import { useHistory } from 'react-router';
+// import axios from "axios";
 
+
+// const UserRegister = (props) => {
+//     const history = useHistory();
+
+//     const [user, setUser] = useState({
+//       userId:0,
+//       userPassword: '',
+//       userName: ''
+        
+//     });
+//     const [oneUser, setOneUser] = useState({
+//         userId:0,
+//         userPassword: '',
+//         userName: ''
+//     });
+
+//     const handleUserData = (evt) => {
+//         console.log("handleUserData", evt.target.name, evt.target.value);
+//         setUser({
+//             ...user,
+//             [evt.target.name]: evt.target.value
+//         });
+//     }
+
+//     const submitRegisterUser = (evt) => {
+//         console.log("submitRegisterUser");
+//         axios.post('http://localhost:8082/UserRegistration', user)
+//             .then((response) => {
+//                 setOneUser(response.data);
+
+//                 alert(`User Registered successfully!`)
+//                 history.push('/home');
+//             }).catch(error => {
+//                 alert('Enter Correct Details!');
+//             });
+//         evt.preventDefault();
+//     }
+
+   
+
+//     return (
+//         <div className="container" >
+//             <div>
+                
+//                 <form className="form form-group row" onSubmit={submitRegisterUser} >
+//                     <div>
+//                     {/* <p>Staff ID</p>
+//                     <input
+//                             type="number"
+//                             id="staffId"
+//                             name="staffId"
+//                             className="form-control mb-3"
+//                             placeholder="Staff Id"
+//                             onChange={handleStaffData}
+//                         /> */}
+//                                                 <p>User Name</p>
+//                         <input
+//                             type="text"
+//                             id="userName"
+//                             name="userName"
+//                             className="form-control mb-3"
+//                             placeholder="User Name"
+//                             onChange={handleUserData}
+//                         />
+//                         <p>User Password</p>
+//                         <input
+//                             type="text"
+//                             id="userPassword"
+//                             name="userPassword"
+//                             className="form-control mb-3"
+//                             placeholder="User Password"
+//                             onChange={handleUserData}
+//                         />
+//                         <input
+//                             type="submit"
+//                             id="submit"
+//                             name="submit"
+//                             className="btn btn-primary mb-3"
+//                             value="Register"
+//                         />
+//                     </div>
+//                 </form>
+               
+//                 {/* <p> {emp.eid} {emp.firstName} {emp.salary} </p> */}
+//             </div>
+//            </div> 
+//     );
+// }
+// export default UserRegister;
 
 import React, { useEffect, useState } from 'react';
 import { useHistory } from 'react-router';
@@ -9,13 +101,13 @@ const UserRegister = (props) => {
     const history = useHistory();
     const [userList, setuserList] = useState([]);
     const [user, setUser] = useState({
-        userId: 0,
-        userPassword: '',
-        userName: ''
-
+      userId:0,
+      userPassword: '',
+      userName: ''
+        
     });
     const [oneUser, setOneUser] = useState({
-        userId: 0,
+        userId:0,
         userPassword: '',
         userName: ''
     });
@@ -43,21 +135,44 @@ const UserRegister = (props) => {
     }
 
 
+
+    const myUserId = (evt) => {
+        console.log(user.userId);
+        axios.get(`http://localhost:8082/user/getAllUsers`)
+            .then((response) => {
+                setuserList(response.data);
+                console.log(userList)
+            }).catch(error => {
+                alert("User ID does not exist!");
+            });
+        evt.preventDefault();
+    }
+
+   
+
     return (
         <div className="container" >
             <div>
-
+                
                 <form className="form form-group row" onSubmit={submitRegisterUser} >
                     <div>
-                        <form>
-                        <p>User Name</p>
+                    {/* <p>Staff ID</p>
+                    <input
+                            type="number"
+                            id="staffId"
+                            name="staffId"
+                            className="form-control mb-3"
+                            placeholder="Staff Id"
+                            onChange={handleStaffData}
+                        /> */}
+                                                <p>User Name</p>
                         <input
                             type="text"
                             id="userName"
                             name="userName"
+                            data-testid="userName"
                             className="form-control mb-3"
                             placeholder="User Name"
-                            data-testid="userName"
                             onChange={handleUserData}
                         />
                         <p>User Password</p>
@@ -65,28 +180,26 @@ const UserRegister = (props) => {
                             type="text"
                             id="userPassword"
                             name="userPassword"
-                            className="form-control mb-3"
-                            placeholder="User Password"
                             data-testid="password"
                             pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
+                            className="form-control mb-3"
+                            placeholder="User Password"
                             onChange={handleUserData}
                         />
                         <input
                             type="submit"
                             id="submit"
                             name="submit"
-                            data-testid="submit"
                             className="btn btn-primary mb-3"
                             value="Register"
                         />
-                        </form>
                     </div>
                 </form>
-               
-
                 
+                    
+                {/* <p> {emp.eid} {emp.firstName} {emp.salary} </p> */}
             </div>
-        </div>
+           </div> 
     );
 }
 export default UserRegister;
